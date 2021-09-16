483a0ad98d087f51c8d83461f7c50952f05960d4
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-16 06:29:38 -0700

    Remove Duplicates from Sorted Array

---
+++ 0026.cpp
@@ -0,0 +1,22 @@
+/* CASE 26: REMOVE DUPLICATES, SORTED ARRAY */
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        // modify the array itself for pass-by-reference
+        // SPECIAL CASE (empty array)
+        if (nums.size() < 1) {
+            return 0;
+        }
+        else { 
+            size_t pointer = 1;
+            for (size_t i = 1; i < nums.size(); ++i) {
+                if (nums[pointer - 1] < nums[i]) {
+                    nums[pointer] = nums[i];
+                    pointer++;
+                }
+            }
+            return pointer;
+        }
+    }
+};
