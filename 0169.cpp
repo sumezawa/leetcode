4279a0f65aa894ab00d252c25f1c17b23330284a
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-20 11:04:53 -0700

    O(n) Space, O(1) Time - Majority Element in Array

---
+++ 0169.cpp
@@ -0,0 +1,34 @@
+/* CASE 169: MAJORITY ELEMENT IN ARRAY */
+// O(n) Time, O(1) Space
+
+#include <vector>
+
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int max = nums[0];
+        int count = 0;
+        
+        // O(1) is possible because majority element
+        // will appear more than half the time
+        // so probabilistically we will get a candidate
+        // that dominates in counts
+        for (uint16_t i = 1; i < nums.size(); ++i) {
+            if (nums[i] == max) {
+                ++count;
+            }
+            else {
+                if (count == -1) { // using 0 breaks
+                    max = nums[i];
+                    ++count;
+                }
+                else {
+                    --count;
+                }
+            }
+        }
+        
+        return max;
+    }
+};
