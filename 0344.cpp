cacca689effc552e8ab65dee67dcd3780432eda3
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-21 10:39:16 -0700

    O(n) Time, O(1) Space, Reverse a String

--- 0344.cpp
+++ 0344.cpp
@@ -1,17 +1,18 @@
 /* CASE 344: REVERSE CHAR ARRAY O(1) SPACE */
 
-// we could have used std::swap from #include <algorithm>
+#include <algorithm>
+// commented out version is still O(1), but it expends a byte more
 
 class Solution {
 public:
     void reverseString(vector<char>& s) {
         uint16_t mid = s.size() / 2;
-        char temp;
+        // char temp;
         for (uint16_t i = 0; i < mid; ++i) {
-            // std::swap(s[i], s[len - i - 1]);
-            temp = s[i];
-            s[i] = s[len - i - 1];
-            s[len - i - 1] = temp;
+            std::swap(s[i], s[len - i - 1]);
+            // temp = s[i];
+            // s[i] = s[len - i - 1];
+            // s[len - i - 1] = temp;
         }
     }
 };
