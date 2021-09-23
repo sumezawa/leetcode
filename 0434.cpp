8a11d8efa8323bdd898b19c793dc9a563f8be8e8
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-23 05:48:01 -0700

    Count # of Words in a String

---
+++ 0434.cpp
@@ -0,0 +1,26 @@
+/* CASE 434: COUNT WORDS */
+// O(n) Time, O(1) Space
+
+
+#include <string>
+
+class Solution {
+public:
+    int countSegments(std::string s) {
+        int counts = 0;
+        bool newword = false;
+        auto it = s.begin();
+        while (it != s.end()) {
+            if (!newword && *it != ' ') {
+                newword = true;
+                ++counts;
+            }
+            else if (newword && *it == ' ') {
+                newword = false;
+            }
+            ++it;
+        }
+        return counts;
+        
+    }
+};
