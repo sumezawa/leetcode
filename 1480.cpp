c568959325edb8baaf64d70e6c5bf82fa99b7adc
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-25 15:02:18 -0700

    Array: C++ Vectors, Running Sum

--- 1480.cpp
+++ 1480.cpp
@@ -1,12 +1,24 @@
 /* CASE 1480: RUNNING SUM */
 
+#include <vector>
+
+
 class Solution {
 public:
-    // RUNNING SUM VECTOR 
-    vector<int> runningSum(vector<int>& nums) {
+    // RUNNING SUM VECTOR -> 
+    std::vector<int> runningSum(std::vector<int>& nums) {
+        std::vector<int> rsum; // std::vectors are arrays that are initialised to 0
+        std::vector<int>::iterator rsit = rsum.begin(); // auto works too
+        
+        // demonstrated below,
+        // vectors can be accessed with the [] operator or by dereferencing iterators
+        *rsit = nums[0];
+        
+        // demonstrated below,
+        // std::vector arrays 1) need not track array size 2) can add as many elements as we want
         for (auto it = nums.begin() + 1; it != nums.end(); ++it) {
-            *it += *(it - 1);
+            *rsit += *(it - 1);
         }
-        return nums;
+        return rsum;
     }
 };
