98f1cc1057d0c97e4996a98468b0b935212b694c
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-25 14:10:22 -0700

    O(n) Time, O(1) Space: Max Element Difference

---
+++ 0121.cpp
@@ -0,0 +1,33 @@
+/* CASE 121: MAX ELEMENT DIFFERENCE */
+// O(n) Time, O(1) Space
+
+#include <stdint.h>
+#include <vector>
+
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        // SPECIAL CASE
+        if (prices.size() < 2) {
+            return 0;
+        }
+        
+        // STANDARD CASE
+        uint16_t min_price = prices[0];
+        uint16_t max_profit = 0;
+        for (int price : prices) {
+            // all we need is
+            // 1) minimum price
+            if (price < min_price) {
+                min_price = price;
+            }
+            // 2) best profit based on
+            // the minimum price so far
+            else if (price - min_price > max_profit) {
+                max_profit = price - min_price;
+            }
+        }
+        return max_profit;  
+    }
+};
