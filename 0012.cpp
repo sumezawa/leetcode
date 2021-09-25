793d029f334913b523b4faeab36f3fb8be4412cf
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-09-25 14:27:23 -0700

    Encoding: Integers to Roman Numeral Strings

--- 0012.cpp
+++ 0012.cpp
@@ -2,17 +2,19 @@
 
 #include <string>
 
+
 class Solution {
 public:
     std::string intToRoman(int num) {
         int digit;
         int exponent = 0;
+        int i;
         std::string ans;
         while (num != 0) {
             digit = num % 10;
             if (exponent == 0) {
                 if (digit < 4) {
-                    for (int i = 0; i < digit; ++i) {
+                    for (i = 0; i < digit; ++i) {
                         ans += "I";
                     }
                 }
@@ -21,7 +23,7 @@ public:
                 }
                 else if (digit > 4 && digit < 9) {
                     ans = "V";
-                    for (int i = digit; i > 5; --i) {
+                    for (i = digit; i > 5; --i) {
                         ans += "I";
                     }
                 }
@@ -33,7 +35,7 @@ public:
             else if (exponent == 1) {
                 std::string t;
                 if (digit < 4) {
-                    for (int i = 0; i < digit; ++i) {
+                    for (i = 0; i < digit; ++i) {
                         t += "X";
                     }
                 }
@@ -42,7 +44,7 @@ public:
                 }
                 else if (digit > 4 && digit < 9) {
                     t = "L";
-                    for (int i = digit; i > 5; --i) {
+                    for (i = digit; i > 5; --i) {
                         t += "X";
                     }
                 }
@@ -55,7 +57,7 @@ public:
             else if (exponent == 2) {
                 std::string h;
                 if (digit < 4) {
-                    for (int i = 0; i < digit; ++i) {
+                    for (i = 0; i < digit; ++i) {
                         h += "C";
                     }
                 }
@@ -64,7 +66,7 @@ public:
                 }
                 else if (digit > 4 && digit < 9) {
                     h = "D";
-                    for (int i = digit; i > 5; --i) {
+                    for (i = digit; i > 5; --i) {
                         h += "C";
                     }
                 }
@@ -75,7 +77,7 @@ public:
                 ++exponent;
             }
             else {
-                for (int i = 0; i < digit; ++i) {
+                for (i = 0; i < digit; ++i) {
                     ans = "M" + ans;
                 }
             }
