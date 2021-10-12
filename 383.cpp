697d093a7f35282d1001f6fdb2e8456f83a2c10b
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2021-10-11 20:22:48 -0700

    Hash Tables: Check if Word Constructable

---
+++ 383.cpp
@@ -0,0 +1,44 @@
+/* CASE 383: LETTER BANK */
+
+#include <string>
+#include <unordered_map> // LETTER BANK
+
+class Solution {
+public:
+    bool canConstruct(std::string ransomNote, std::string magazine) {
+        // SPECIAL CASES
+        if (ransomNote.size() > magazine.size()) {
+            return false;
+        }
+        
+        // STANDARD CASES
+        std::unordered_map<char, int> letter_bank;
+        std::string::iterator it = magazine.begin();
+        while (it != magazine.end()) {
+            if (letter_bank.find(*it) != letter_bank.end()) {
+                ++letter_bank[*it];
+            }
+            else {
+                letter_bank[*it] = 1;
+            }
+            ++it;
+        }
+        
+        it = ransomNote.begin();
+        while (it != ransomNote.end()) {
+            if (letter_bank.find(*it) != letter_bank.end()) {
+                if (letter_bank[*it] > 0) {
+                    --letter_bank[*it];
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+            ++it;
+        }
+        return true;
+    }
+};
