8a038bce6cd0bdbf1e77821c889d4190a92e946f
Sammy Umezawa <sammyumezawa@umail.ucsb.edu>
2022-01-06 00:20:06 -0800

    Strings: Find First Palindrome in String Array

--- 2108.cpp
+++ 2108.cpp
@@ -28,4 +28,34 @@ public:
         }
         return "";
     }
+
+    // less variables
+    // less operations per loop
+    std::string improved_firstPalindrome(std::vector<std::string>& words) {
+        bool found_palindrome = false;
+        std::size_t word_size;
+        for (std::string& word : words) {
+            word_size = word.size();
+            // special case: one-letter word is always palindrome
+            if (word_size == 1) {
+                return word;
+            }
+            // e.g.
+            // aba 2 check up to a 0 (n < index 1)
+            // aabbaa 5 check up to aab 2 (n < index 3)
+            
+            for (std::size_t i = 0; i < word_size / 2; ++i) {
+                if (word[i] == word[word_size - 1 - i]) {
+                    if (i == word_size / 2 - 1) {
+                        return word;    
+                    }
+                    
+                }
+                else {
+                    break; // not palindrome
+                }
+            }
+        }
+        return "";
+    }
 };
